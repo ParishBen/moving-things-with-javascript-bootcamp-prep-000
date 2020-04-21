8514de5851c33db62ba021dc2f7dd65bc0768d09
Ben Parish <parishben13@gmail.com>
2020-04-21 15:55:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = "#000000";
+dodger.style.backgroundColor = '#FF69B4';
+dodger.style.left;
